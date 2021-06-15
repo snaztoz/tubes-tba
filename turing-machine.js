cfb59c5bc19611eb528f30ee38c2ae8a0710658f
snaztoz <hafidhmn@gmail.com>
2021-06-15 16:30:13 +0700

    Membuat validasi input form.

--- turing-machine.js
+++ turing-machine.js
@@ -18,6 +18,19 @@ class Form
     constructor(formEl)
     {
         this.el = formEl
+        this.operationValidationRules = {
+            'penjumlahan': ['required-both'],
+            'pengurangan': ['required-both'],
+            'perkalian': ['required-both'],
+            'pembagian': ['required-both'],
+
+            'faktorial': ['required-first', 'positive'],
+
+            'modulo': ['required-both', 'positive'],
+            'perpangkatan': ['required-both', 'positive'],
+            'logaritma-biner': ['required-both', 'positive'],
+        }
+
         this.init()
     }
 
@@ -25,7 +38,82 @@ class Form
     {
         $(`${this.el} #form-submit`).click(event => {
             event.preventDefault()
-            console.log('CLICKED')
+
+            const {isValid, data} = this.validate({
+                jenisOperasi: $(`${this.el} #jenis-operasi`).val(),
+                bilangan1: $(`${this.el} #bilangan-1`).val().trim(),
+                bilangan2: $(`${this.el} #bilangan-2`).val().trim(),
+            })
+
+            console.log('Valid: ' + isValid)
+            console.log('err: ' + data.err)
         })
     }
+
+    /**
+     * Menjalankan validasi untuk elemen operasi yang diberikan.
+     *
+     * Akan mengembalikan object yang berisikan:
+     *
+     *      isValid -> valid atau tidaknya operasi
+     *      data    -> jika valid, akan berisikan data elemen operasi
+     *                 jika error, akan berisikan pesan error
+     */
+    validate(operationElements)
+    {
+        let isValid = false;
+
+        if (!this.operationValidationRules
+                .hasOwnProperty(operationElements.jenisOperasi))
+        {
+            return {isValid, data: {'err': 'unknown operation'}}
+        }
+
+        const jenisOperasi = operationElements.jenisOperasi
+        const bilangan1 = operationElements.bilangan1
+        const bilangan2 = operationElements.bilangan2
+
+        const rules = this.operationValidationRules[jenisOperasi]
+
+        for (const rule of rules)
+        {
+            const isRuleValidationPass = this.runRuleValidation(rule, bilangan1, bilangan2)
+            if (!isRuleValidationPass)
+            {
+                return {isValid, data: {'err': 'invalid input'}}
+            }
+        }
+
+        isValid = true
+
+        return {
+            isValid,
+            data: {jenisOperasi, bilangan1, bilangan2}
+        }
+    }
+
+    runRuleValidation(rule, bilangan1, bilangan2)
+    {
+        if (rule === 'required-both')
+        {
+            return isIntegerString(bilangan1) && isIntegerString(bilangan2)
+        }
+        else if (rule === 'required-first')
+        {
+            return isIntegerString(bilangan1)
+        }
+        else if (rule === 'positive')
+        {
+            return parseInt(bilangan1) > 0 && parseInt(bilangan2) > 0
+        }
+
+        throw new Error('unknown validation rule')
+    }
+}
+
+// Memeriksa apakah string yang diberikan berisikan sebuah
+// value integer yang valid.
+function isIntegerString(text)
+{
+    return !isNaN(text) && parseInt(text) == text
 }
