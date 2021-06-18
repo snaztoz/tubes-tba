b909bf2aad7d1c5690076fd8a7f0ea485d89994d
F-Pawamatra <72060401+F-Pawamatra@users.noreply.github.com>
2021-06-18 19:08:31 +0700

    memasukkan pengurangan

--- turing-machine-operations.js
+++ turing-machine-operations.js
@@ -77,7 +77,7 @@ const operations = {
             'Q3': [
                 {
                     'receive': 'X',
-                    'move': 'right -> done'
+                    'move': 'right -> end'
                 },
                 {
                     'receive': '0',
@@ -86,7 +86,7 @@ const operations = {
                 },
                 {
                     'receive': 'Y',
-                    'move': 'right -> done'
+                    'move': 'right -> end'
                 },
             ],
 
@@ -136,7 +136,7 @@ const operations = {
                 },
                 {
                     'receive': '1',
-                    'move': 'right -> done'
+                    'move': 'right -> end'
                 },
             ],
             
@@ -194,22 +194,22 @@ const operations = {
                 },
                 {
                     'receive': 'X',
-                    'move': 'right -> done'
+                    'move': 'right -> end'
                 },
                 {
                     'receive': 'Y',
-                    'move': 'right -> done'
+                    'move': 'right -> end'
                 },
             ],
             
             'Q11': [
                 {
                     'receive': 'X',
-                    'move': 'left -> done'
+                    'move': 'left -> end'
                 },
                 {
                     'receive': 'Y',
-                    'move': 'left -> done'
+                    'move': 'left -> end'
                 },
                 {
                     'receive': '1',
@@ -296,11 +296,319 @@ const operations = {
                 },
                 {
                     'receive': 'B',
-                    'move': 'left -> done'
+                    'move': 'left -> end'
                 },
             ],
             
-            'done': []
+            'end': []
+        }
+    },
+
+    'pengurangan': 
+    {
+        'start-state': 'start',
+        'end-state': 'end',
+
+        'states':
+        {
+            'start': [
+                {
+                    'receive': 'X',
+                    'move': 'right -> Q1'
+                },
+                {
+                    'receive': 'Y',
+                    'move': 'right -> Q2'
+                },
+                {
+                    'receive': 'Z',
+                    'write': 'B',
+                    'move': 'right -> Q3'
+                }
+            ],
+            
+            'Q1': [
+                {
+                    'receive': '1',
+                    'move': 'right'
+                },
+                {
+                    'receive': '0',
+                    'move': 'right'
+                },
+                {
+                    'receive': 'X',
+                    'write': 'Y',
+                    'move': 'left -> Q5'
+                },
+                {
+                    'receive': 'Y',
+                    'write': '0',
+                    'move': 'right -> Q4'
+                },
+                {
+                    'receive': 'Z',
+                    'write': 'B',
+                    'move': 'left -> Q6'
+                }
+            ],
+            
+            'Q2': [
+                {
+                    'receive': '1',
+                    'move': 'right'
+                },
+                {
+                    'receive': '0',
+                    'move': 'right'
+                },
+                {
+                    'receive': 'X',
+                    'write': '0',
+                    'move': 'right -> Q4'
+                },
+                {
+                    'receive': 'Y',
+                    'write': 'X',
+                    'move': 'left -> Q5'
+                },
+                {
+                    'receive': 'Z',
+                    'write': 'B',
+                    'move': 'left -> Q6'
+                }
+                
+            ],
+
+            'Q3': [
+                {
+                    'receive': 'X',
+                    'write': 'Y',
+                    'move': 'right -> end'
+                },
+                {
+                    'receive': '0',
+                    'write': 'B',
+                    'move': 'right'
+                },
+                {
+                    'receive': 'Y',
+                    'write': 'X',
+                    'move': 'right -> end'
+                },
+            ],
+
+            'Q4': [
+                {
+                    'receive': '1',
+                    'write': '0',
+                    'move': 'left -> Q7'
+                },
+                {
+                    'receive': '0',
+                    'move': 'right'
+                },
+                {
+                    'receive': 'B',
+                    'move': 'left -> Q6'
+                },
+            ],
+
+            'Q5': [
+                {
+                    'receive': '0',
+                    'move': 'left'
+                },
+                {
+                    'receive': '1',
+                    'write': '0',
+                    'move': 'right -> Q9'
+                },
+                {
+                    'receive': 'X',
+                    'write': 'B',
+                    'move': 'right -> Q10'
+                },
+                {
+                    'receive': 'Y',
+                    'write': 'B',
+                    'move': 'right -> Q10'
+                },
+            ],
+            
+            'Q6': [
+                {
+                    'receive': '0',
+                    'write': 'B',
+                    'move': 'left'
+                },
+                {
+                    'receive': '1',
+                    'move': 'right -> end'
+                },
+            ],
+            
+            'Q7': [
+                {
+                    'receive': '1',
+                    'move': 'right -> Q8'
+                },
+                {
+                    'receive': '0',
+                    'move': 'left'
+                },
+                {
+                    'receive': 'B',
+                    'move': 'left'
+                },
+            ],
+            
+            'Q8': [
+                {
+                    'receive': '0',
+                    'write': '1',
+                    'move': 'right -> Q4'
+                },
+            ],
+            
+            'Q9': [
+                {
+                    'receive': '0',
+                    'move': 'right'
+                },
+                {
+                    'receive': 'X',
+                    'move': 'right'
+                },
+                {
+                    'receive': 'Y',
+                    'move': 'right'
+                },
+                {
+                    'receive': '1',
+                    'move': 'right'
+                },
+                {
+                    'receive': 'B',
+                    'move': 'left -> Q11'
+                },
+            ],
+            
+            'Q10': [
+                {
+                    'receive': '0',
+                    'write': 'B',
+                    'move': 'right -> Q10'
+                },
+                {
+                    'receive': 'X',
+                    'move': 'right -> end'
+                },
+                {
+                    'receive': 'Y',
+                    'move': 'right -> end'
+                },
+            ],
+            
+            'Q11': [
+                {
+                    'receive': 'X',
+                    'move': 'left -> end'
+                },
+                {
+                    'receive': 'Y',
+                    'move': 'left -> end'
+                },
+                {
+                    'receive': '1',
+                    'write': 'B',
+                    'move': 'left -> Q12'
+                },
+            ],
+            
+            'Q12': [
+                {
+                    'receive': '1',
+                    'move': 'left -> Q13'
+                },
+                {
+                    'receive': 'X',
+                    'move': 'left -> Q14'
+                },
+                {
+                    'receive': 'Y',
+                    'move': 'left -> Q14'
+                },
+            ],
+            
+            'Q13': [
+                {
+                    'receive': '1',
+                    'move': 'left'
+                },
+                {
+                    'receive': 'X',
+                    'move': 'left'
+                },
+                {
+                    'receive': 'Y',
+                    'move': 'left'
+                },
+                {
+                    'receive': '0',
+                    'move': 'left -> Q5'
+                },
+            ],
+            
+            'Q14': [
+                {
+                    'receive': '0',
+                    'move': 'left'
+                },
+                {
+                    'receive': '1',
+                    'move': 'right -> Q15'
+                },
+                {
+                    'receive': 'X',
+                    'write': 'Z',
+                    'move': 'right -> Q15'
+                },
+                {
+                    'receive': 'Y',
+                    'write': 'Z',
+                    'move': 'right -> Q15'
+                },
+            ],
+            
+            'Q15': [
+                {
+                    'receive': '0',
+                    'write': 'B',
+                    'move': 'right'
+                },
+                {
+                    'receive': '1',
+                    'write': 'B',
+                    'move': 'right'
+                },
+                {
+                    'receive': 'X',
+                    'write': 'B',
+                    'move': 'right'
+                },
+                {
+                    'receive': 'Y',
+                    'write': 'B',
+                    'move': 'right'
+                },
+                {
+                    'receive': 'B',
+                    'move': 'left -> end'
+                },
+            ],
+            
+            'end': []
         }
     },
 }
